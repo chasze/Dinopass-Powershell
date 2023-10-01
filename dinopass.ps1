ae6aaba5b2ff6e90accda9ff2d3dadcab31d3838
chasze <chasze@gmail.com>
2023-09-30 20:01:07 -0400

    Update dinopass.ps1

--- dinopass.ps1
+++ dinopass.ps1
@@ -1,34 +1,58 @@
 #Gets a random password from dinopass api
 
+
 New-Variable -Name "password" -Value (Invoke-WebRequest 'https://www.dinopass.com/password/strong').Content
 
 #prompts for username
 $user = Read-Host "Please enter username or email"
  
-$password >> ~\Desktop\password.txt
 
  #copies the password to clipboard
  $password | clip	
 
-#Prints to console
+
+$folderCheck = Test-Path -Path "~\Desktop\Dinopass"
+
+
+function GeneratePass { 
+    if(Test-Path -Path "~\Desktop\Dinopass\$user.txt") 
+    {   
+        $response = Read-Host "$user already exist, password will be overwritten type Y to proceed or any other to cancel"
+                if ($response -match "Y" -or $response -match "y") {
+                    write-Output ("Username: $user 
+    Password: $password") > ~\Desktop\Dinopass\$user.txt
+
+
+
+                    
+    
+                }
+                else {
+                   break
+                }
+ } }
+
+
+
+if (-Not $folderCheck) {
+    mkdir ~\Desktop\Dinopass
+    GeneratePass }
+
+
+
+if ($folderCheck) { 
+    GeneratePass
+}
+
+
+
 
 
 
 write-Output ("Username: $user 
-Password: $password") 
-
-
-
-if (Test-Path -Path "~\Desktop\Dinopass")
-{
-    write-Output ("Username: $user 
-Password: $password") > ~\Desktop\Dinopass\$user.txt
-}
-else {
-    mkdir ~\Desktop\Dinopass
-    write-Output ("Username: $user 
 Password: $password") > ~\Desktop\Dinopass\$user.txt
 
-}
+write-Output ("Username: $user 
+Password: $password")
 
-
+#why did I spend so much time making this?
